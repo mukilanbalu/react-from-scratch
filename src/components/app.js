17498980c1bcd56d854a08998b480c80a06647d8
Mukilan Balasubramanian <Mukilan.B@netserv.local>
2019-09-14 13:22:50 +0530

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class App extends Component{
+        render(){
+            return(
+                <h2>Test</h2>
+            )
+        }
+}
+export default App;
